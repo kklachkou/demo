1a2821f605c0339ea98205b12976166e935c3c69
Kiryl Klachkou <kiryl.klackou@gmail.com>
2021-02-20 23:38:58 +0200

    oome

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import io.vavr.control.Try;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("hello")
@@ -13,6 +17,16 @@ public class Controller {
     @GetMapping
     public Greetings hello() {
         return Greetings.builder().message("Hello Dr.K!").build();
+    }
+
+    @GetMapping("/oome")
+    public void oome() {
+        Try.run(this::oome3);
+    }
+
+    private void oome3() {
+        List<Byte> list = new ArrayList<>();
+        while (true) list.add((byte) 1);
     }
 
     @Data
